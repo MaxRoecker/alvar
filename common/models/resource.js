5ec5a4c7fa9695ddd17459c99b480f9f560dce96
joao <jlramalheira@gmail.com>
2015-10-05 21:20:45 -0300

    Web Service with DeCS.

--- common/models/resource.js
+++ common/models/resource.js
@@ -1,3 +1,38 @@
+var request = require("request");
+var parseString = require("xml2js").parseString;
+var iconv = require("iconv-lite");
+
 module.exports = function(Resource) {
-  //teste
+
+  Resource.searchDecs = function(search, cb) {
+    var url = "http://decs.bvsalud.org/cgi-bin/mx/cgi=@vmx/decs/?words=" + search;
+    var requestOptions  = { encoding: null, method: "GET", uri: url};
+    request(requestOptions, function(err, response, body) {
+      var body = iconv.decode(new Buffer(body), "ISO-8859-1");
+      parseString(body,function(err,result){
+        var decs = [];
+        var responses = result.decsvmx.decsws_response;
+        responses.forEach(function (elem){
+          var aux = {};
+          aux.term = elem.tree[0].self[0].term_list[0].term[0]._;
+          aux.synonyms = elem.record_list[0].record[0].synonym_list[0].synonym;
+          decs.push(aux);
+        });
+        cb(null,decs);
+      });
+    });
+  }
+
+  Resource.remoteMethod(
+    'searchDecs', {
+      accepts: {
+        arg: 'search',
+        type: 'string'
+      },
+      returns : {
+        arg: 'result',
+        type: 'object'
+      }
+    }
+  );
 };
