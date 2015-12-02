f5a99ae537d6d482d2675b9d6aac6dc22fd0923b
joao <jlramalheira@gmail.com>
2015-12-02 17:30:07 -0200

    Use mysql connection.

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,11 +1,13 @@
-module.exports = function(app) {
-  var User = app.models.User;
-
-  User.create({username: 'admin', email: 'admin@alvar.com', password: 'opensesame'}, function(err, user) {
-      console.log(user);
-
-      User.login({username: 'admin', password: 'opensesame'}, function (err, accessToken){
-         console.log('access token: '+accessToken.id); 
-      });
-  });
-};
+// module.exports = function(app) {
+//
+//   var User = app.models.User;
+//
+//   User.create({username: 'admin', email: 'admin@alvar.com', password: 'opensesame'}, function(err, user) {
+//       console.log(user);
+//
+//       User.login({username: 'admin', password: 'opensesame'}, function (err, accessToken){
+//          console.log('access token: '+accessToken.id);
+//       });
+//   });
+//
+// };
